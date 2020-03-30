7105ea29049ec873068cd9455b4d518a745be9cd
dataiyangu <32248827@qq.com>
2020-03-30 15:11:46 +0800

    v2.0 spring、注解、版本升级

---
+++ src/main/java/com/leesin/IpaymentService.java
@@ -0,0 +1,12 @@
+package com.leesin;
+
+/**
+ * @description:
+ * @author: Leesin.Dong
+ * @date: Created in 2020/3/30 14:53
+ * @version: ${VERSION}
+ * @modified By:
+ */
+public interface IpaymentService {
+    public String dopay();
+}
